f24a8502889659a0768051031ba49f75ab1eae10
Ahmed IOS Dev <ahmediosdev@Ahmeds-Mac.local>
2017-10-07 07:09:25 -0700

    Initial Commit

---
+++ Ideas2Tech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ideas2Tech
+//
+//  Created by Ahmed IOS Dev on 10/7/17.
+//  Copyright © 2017 Ahmed IOS Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
